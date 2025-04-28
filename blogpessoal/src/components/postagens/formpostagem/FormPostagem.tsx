0be2d1f6bc82bf8ea48a332f665defbda273a5f2
EvelynSantos6 <evelyn.santos20032@gmail.com>
2025-04-28 12:13:33 -0300

    Atualizações projeto

--- blogpessoal/src/components/postagens/formpostagem/FormPostagem.tsx
+++ blogpessoal/src/components/postagens/formpostagem/FormPostagem.tsx
@@ -189,7 +189,7 @@ function FormPostagem() {
                 <button
                     type='submit'
                     className='rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800
-                              text-white font-bold w-1/2 mx-auto py-2 flex justify-center'
+                            text-white font-bold w-1/2 mx-auto py-2 flex justify-center'
                     disabled={carregandoTema}
                 >
                     {isLoading ?
